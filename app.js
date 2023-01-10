f3e8da65d7e8abeaa3f1f4029f17afc465cff2bf
saidalixon-sobirov <saidalixon_sobirov@mail.ru>
2023-01-10 21:28:24 +0500

    1-dars. for lesson

---
+++ app.js
@@ -0,0 +1,111 @@
+"use strict"; 
+
+
+// DATA TYPES  -  2 guruhga
+//  primitive
+//  non-primitive
+
+// //  primitive
+// number  -   24,12, -45.6
+// string   -  "salom", "34"
+// Boolean  -  true, fasle;
+// undefined  - undefined
+// null  - null 
+// bigInt  - 12312312433241234123423n 
+// symbol   - Symbol("id");  
+
+// //  non-primitive
+// Object   -- function, Object, array 
+
+// Variables -  let, const, var
+
+
+// let A = 45;
+// a  = 23;
+
+// console.log(a);
+
+
+// const pi = 3.14;
+
+// console.log(pi);
+
+
+// pi = "sasas";
+
+// console.log(pi);
+
+
+
+// "use strict";
+
+//  alert, confirm, prompt
+
+
+// alert("tushunarlimi?")
+
+// confirm("rostanmi>")
+
+// prompt("yoshni kirit")
+
+
+
+// interpolyatsiya    ` ${variable_name} `
+
+
+// let age = prompt(" yoshingizni kiriting...")
+// let name = prompt(" ismingizni kiriting...")
+
+
+// // console.log("sizning yoshingiz " + age + " da!");
+
+// console.log(  `Salom ${name} tanishganimdan xursandman. sizning yoshingiz ${age} da ekan. meni yoshim esa ${age - 4} da`  )  // backtick -  ``
+
+
+// typeof - show data-type 
+
+// let a = 23;
+// console.log(typeof a);
+
+
+// let b = "23";
+// console.log(typeof b);
+
+
+// console.log(typeof 32);
+
+// console.log(typeof "salom");
+
+// console.log(typeof true);
+
+// console.log(typeof undefined);
+
+// console.log(typeof null);
+
+// console.log(typeof {"ism": "ali"});
+
+// console.log(typeof 12n);
+
+// console.log(typeof Symbol("id"));
+
+
+  // let a = "asd"
+
+  // console.log(a);
+  // console.log(typeof a);
+
+  // console.log(Number(a));
+  // console.log(typeof Number(a));
+  //  NaN - Not a Number
+
+
+  let a = "  ";
+
+  console.log(a);
+  console.log(typeof a);
+
+
+  const b  = Boolean(a)
+  console.log(b);
+  console.log(typeof b);
+  
